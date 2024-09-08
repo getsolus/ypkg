6749518074f3fdc70b4ea10bc6d960dfa95bc9e9
Joey Riches <josephriches@gmail.com>
2024-09-08 01:25:40 +0100

    Revert "ypkg2: Only enable fakeroot during the install & check steps"
    This reverts commit bcf1b168bbb8034f25567862ffd1b2ec3a18f690.

--- ypkg2/main.py
+++ ypkg2/main.py
@@ -150,12 +150,6 @@ def execute_step(context, step, step_n, work_dir):
     if context.avx2 and step_n == "install":
         endScript = "%avx2_lib_shift"
 
-    # We only want fakeroot for the install and check steps due
-    # to massive performance overhead.
-    # This is a quick n' dirty way of achieving that
-    if not step_n in ["install", "check"]:
-        script.define_unexport("LD_PRELOAD")
-
     exports = script.emit_exports()
 
     # Run via bash with enable and error
