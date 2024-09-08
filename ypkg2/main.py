7a72c3ce075392eefd34978ad52135ae6a27af63
Joey Riches <josephriches@gmail.com>
2024-09-08 01:23:55 +0100

    Revert "ypkg2: Add optional bool for fatfakeroot"
    This reverts commit e7b70309c5e8e0b184ed4784ba4d1383a8b3046d.

--- ypkg2/main.py
+++ ypkg2/main.py
@@ -150,9 +150,10 @@ def execute_step(context, step, step_n, work_dir):
     if context.avx2 and step_n == "install":
         endScript = "%avx2_lib_shift"
 
-    # Generally, we only want fakeroot for the install and check steps due
-    # to the massive performance overhead unless fatfakeroot is requested.
-    if not step_n in ["install", "check"] and not context.spec.pkg_fatfakeroot:
+    # We only want fakeroot for the install and check steps due
+    # to massive performance overhead.
+    # This is a quick n' dirty way of achieving that
+    if not step_n in ["install", "check"]:
         script.define_unexport("LD_PRELOAD")
 
     exports = script.emit_exports()
