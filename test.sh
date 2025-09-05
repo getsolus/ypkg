2bb178332d43edc86a885eae55dbdae29e091f2d
Evan Maddock <maddock.evan@vivaldi.net>
2025-09-05 15:15:17 -0400

    test: Clean up testing scripts
    Now that we are PEP517, we can simplify setting up the `venv` for
    testing. We also don't need to use special `git` branches since both
    `ypkg` and `eopkg` are Python 3 by default.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pep8 ypkg-build ypkg2/*.py ypkg-gen-history ypkg-install-deps ypkg || exit 1
-#for item in examples/*.yml ; do
-#    python -m ypkg2.main $item || exit 1
-#done
-
-fakeroot ./ypkg-build examples/nano.yml || exit 1
