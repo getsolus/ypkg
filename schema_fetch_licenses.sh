d9fd4d9f51377d01b1ca578f6d85a096bffb4b2f
Thomas Staudinger <Staudi.Kaos@gmail.com>
2025-12-15 02:15:27 +0100

    licenses: Improve fetch script and update license list
    Don't fetch deprecated licenses and adjust to new directory structure
    
    Signed-off-by: Thomas Staudinger <Staudi.Kaos@gmail.com>
    (cherry picked from commit 62ffca93c0df1e446302f3d5289c1991076cf10c)

--- schema_fetch_licenses.sh
+++ schema_fetch_licenses.sh
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json |
-jq '{enum: [.licenses[].licenseId]}' |
-jq '.enum += ["EULA", "Distributable", "Public-Domain"]' > data/licenses.json
+jq '{enum: [(.licenses[] | select(.isDeprecatedLicenseId == false)).licenseId]}' |
+jq '.enum += ["EULA", "Distributable", "Public-Domain"]' > data/schema/licenses.json
